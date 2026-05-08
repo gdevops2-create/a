991525b110a9d087d03e03146c019840681b30cf
gdevops2-create <gdevops2@gmail.com>
2026-05-08 08:36:49 +0530

    Update text.py

--- text.py
+++ text.py
@@ -1,30 +1,85 @@
-file=None
+file = None
+
 while True:
-    print("1,2,3,4,5")
-    ch=int(input("Enter choice"))
+
+    print("\n1.Create File")
+    print("2.Write File")
+    print("3.Read File")
+    print("4.Delete File")
+    print("5.Exit")
+
+    ch = int(input("Enter choice : "))
 
     try:
-        if ch==1:
-            name=input("enter file name")
-            file=open(name,'w+')
-        elif ch==2:
-            name=input("enter file name")
-            text=input("enter text")
-            file=open(name,'w+')
-            file.write(text,'w+')
-        elif ch==3:
-            name=input("enter file name")
-            file=open(name,'w+')
-        elif ch==4:
-            name=input("enter file name")
-            file=open(name,'w+')
 
-        
-    
+        # CREATE FILE
+        if ch == 1:
+
+            name = input("Enter file name : ")
+
+            file = open(name, 'w')
+            print("File created successfully")
+
+            file.close()
+
+        # WRITE FILE
+        elif ch == 2:
+
+            name = input("Enter file name : ")
+
+            text = input("Enter text : ")
+
+            file = open(name, 'a')
+
+            file.write(text + "\n")
+
+            print("Text written successfully")
+
+            file.close()
+
+        # READ FILE
+        elif ch == 3:
+
+            name = input("Enter file name : ")
+
+            file = open(name, 'r')
+
+            print("\nFile Content :\n")
+
+            print(file.read())
+
+            file.close()
+
+        # DELETE FILE
+        elif ch == 4:
+
+            import os
+
+            name = input("Enter file name : ")
+
+            os.remove(name)
+
+            print("File deleted successfully")
+
+        # EXIT
+        elif ch == 5:
+
+            print("Program Ended")
+
+            break
+
+        else:
+
+            print("Invalid Choice")
+
     except FileNotFoundError:
-        print("file not found")
-    except Exception as e:
-        print("exception error")
+
+        print("File not found")
 
     except IOError:
-        print("io error")
+
+        print("IO Error")
+
+    except Exception as e:
+
+        print("Exception Error :", e)
